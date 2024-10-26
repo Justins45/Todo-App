53641505fcd646dcef8bf8104c5859c66081c10c
Justin Shaw <shaw.justin@shaw.ca>
2024-10-26 15:06:13 -0600

    add listgroup component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ListItem from '@/components/ListItem.vue'
+import ListGroup from '@/components/ListGroup.vue'
 import { PlusIcon } from '@heroicons/vue/24/solid'
 
 function runTest() {
@@ -14,23 +14,5 @@ function runTest() {
       <PlusIcon class="h-10 w-10 text-green-500" />
     </button>
   </div>
-  <!-- List group start -->
-  <div class="px-5 py-3">
-    <!-- List title -->
-    <h2 class="h-10 uppercase">Make To-do app</h2>
-    <div class="h-0.5 w-full rounded-xl bg-zinc-300"></div>
-    <!-- List container start -->
-    <ul class="ml-3 mt-3 lowercase">
-      <!-- List items -->
-      <ListItem name="make CONTAINERS" id="item1" />
-      <!-- List items -->
-      <ListItem name="make components" id="item" />
-      <!-- List items -->
-      <ListItem name="make styling" id="item3" />
-      <!-- List items -->
-      <ListItem name="combine all items" id="item4" />
-    </ul>
-    <!-- List containter end -->
-  </div>
-  <!-- List group end -->
+  <ListGroup />
 </template>
