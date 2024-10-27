522bffd3b4d3bb2f4675e85ab1fe378cc1e4263f
Justin Shaw <shaw.justin@shaw.ca>
2024-10-27 14:13:38 -0600

    add data to new json dada sheet

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,15 +4,30 @@ import { PlusIcon } from '@heroicons/vue/24/solid'
 import ListGroup from '@/components/ListGroup.vue'
 import TodoData from '../assets/data.json'
 
-const text = ref()
-
+const new_group_name = ref('')
+const err = ref()
 function runTest() {
   alert('test ran')
 }
 
 function onSubmit() {
-  alert(text.value)
-  text.value = ''
+  if (new_group_name.value !== '') {
+    err.value = ''
+
+    TodoData.push({
+      id: '3',
+      group_name: new_group_name.value,
+      data: [],
+    })
+
+    const new_data_sheet = JSON.stringify(TodoData, null, 4)
+
+    console.log('new data sheet', new_data_sheet)
+
+    new_group_name.value = ''
+  } else {
+    err.value = 'please input a value'
+  }
 }
 
 // TODO: get form working for adding new todo groups | + make form a modal / popup
@@ -32,9 +47,12 @@ function onSubmit() {
         type="text"
         placeholder="Todo Group Name"
         id="todo-group-name-input"
-        v-model="text"
+        v-model="new_group_name"
         class="rounded-md border border-zinc-500 p-1"
       />
+      <template v-if="err">
+        <p class="p-1 font-bold text-red-500">{{ err }}</p>
+      </template>
       <button type="submit" class="w-min rounded-md bg-blue-300 p-1">
         Submit
       </button>
