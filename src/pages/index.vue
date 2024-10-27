d3963b06ecfa34e53c9199e90fdf88d732c8fa91
Justin Shaw <shaw.justin@shaw.ca>
2024-10-27 15:02:01 -0600

    add post request - not working, blocked by cors

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,12 +12,13 @@ function runTest() {
   alert('test ran')
 }
 
-function onSubmit() {
+async function onSubmit() {
   if (new_group_name.value !== '') {
     // remove any error messages present
     err.value = ''
 
     // add data to json file
+    // TODO: add pina store to keep track of id number increasing + setting default to new "top" number each time
     TodoData.push({
       id: '3',
       group_name: new_group_name.value,
@@ -26,6 +27,19 @@ function onSubmit() {
 
     const new_data_sheet = JSON.stringify(TodoData, null, 4)
     console.log('new data sheet', new_data_sheet)
+
+    // FIXME: cors error not allowing json post - Axios / npm cors install might be needed + moving request to a .js file
+
+    await fetch('http://127.0.0.1:8080/data.json', {
+      method: 'POST',
+      mode: 'no-cors',
+      body: new_data_sheet,
+    })
+
+    const new_new_data = await fetch('http://127.0.0.1:8080/data.json')
+    console.log('NEW NEW NEW DATA SHEET', JSON.stringify(new_new_data, null, 4))
+
+    // TODO: get data to stay in local json file and not in local json file - NOTE: might need a local server to run for this
 
     // remove input value
     new_group_name.value = ''
