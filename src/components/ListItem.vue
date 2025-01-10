6ea7e8a235c08f98ddde8e18b0d2737d2a0d51b5
Justin Shaw <shaw.justin@shaw.ca>
2025-01-09 20:25:42 -0700

    start delete group item function

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { TrashIcon } from '@heroicons/vue/24/outline'
 
 const props = defineProps<{
   id: string
@@ -11,12 +12,23 @@ const checked = ref(props.completed)
 // TODO: add UUID for each list item
 // TODO: when completed update data.json at corrent location
 // TODO: when completed move to bottom of group
+
+const deleteItem = () => {
+  console.log(`Deleting item ${props.todo_title}`)
+}
 </script>
 <template>
-  <input type="checkbox" :id="props.id" class="mr-2" v-model="checked" />
-  <label
-    :for="props.id"
-    :class="[checked ? 'text-zinc-400 line-through' : '']"
-    >{{ props.todo_title }}</label
-  >
+  <div class="flex justify-between">
+    <div>
+      <input type="checkbox" :id="props.id" class="mr-2" v-model="checked" />
+      <label
+        :for="props.id"
+        :class="[checked ? 'text-zinc-400 line-through' : '']"
+        >{{ props.todo_title }}</label
+      >
+    </div>
+    <div @click="deleteItem(props.todo_title)">
+      <TrashIcon class="h-5 w-5 text-red-500 hover:underline" />
+    </div>
+  </div>
 </template>
